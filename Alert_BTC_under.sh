1cecb3eef65e22fd692f49ee0822d490c9f472e5
Andres E Sampaulo <50955035+asampau@users.noreply.github.com>
2020-08-02 15:26:06 -0300

    Update Alert_BTC_under.sh
    Some changes correcting get prices from website

--- Alert_BTC_under.sh
+++ Alert_BTC_under.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-trishold=$1
+threshold=$1
 money=Bitcoin
+interval=600		# Interval between checks in seconds
 this=`basename $0 | sed 's/.sh//'`
-[ $# -lt 1 ] && echo "Usage: ./$this <trishold>" && exit 1;
-BT=`lynx -dump 'https://es.tradingview.com/markets/cryptocurrencies/prices-all/' | grep 'Bitcoin [0-9]' | awk '{print int($5)}'`
-echo "scale=2;$BT / $trishold" | bc
-while [ $BT -gt $trishold ]; 
+[ $# -lt 1 ] && echo "Usage: ./$this <threshold>" && exit 1;
+# Complete List is: lynx -dump 'https://tradingview.com/markets/cryptocurrencies/prices-all/' | awk '!/[0-9]\][A-Z]/{if ($NF ~ /\%$/) {print $0}}'
+BT=`lynx -dump 'https://tradingview.com/markets/cryptocurrencies/prices-all/' | awk '!/[0-9]\][A-Z]/{if ($NF ~ /\%$/) {print $0}}' | head -1 | awk '{print int($3)}'`
+echo "scale=2;$BT / $threshold" | bc
+while [ $BT -gt $threshold ]; 
    do 
 	date; 
-	BT=`lynx -dump 'https://es.tradingview.com/markets/cryptocurrencies/prices-all/' | grep 'Bitcoin [0-9]' | awk '{print int($5)}'`; 
+	BT=`lynx -dump 'https://tradingview.com/markets/cryptocurrencies/prices-all/' | awk '!/[0-9]\][A-Z]/{if ($NF ~ /\%$/) {print $0}}' | head -1 | awk '{print int($3)}'`;
 	banner $BT; 
-	sleep 600; 
+	sleep $interval; 
    done; 
-mailx -s $this_$BT asampau@gmail.com < /dev/null
+mailx -s $this_$BT your@email.com < /dev/null
